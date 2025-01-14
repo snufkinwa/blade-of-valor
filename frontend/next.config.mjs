e062cf16e263562804d55e3b6cecf0f2b8dbaee9
Janay Harris <jha2972@wgu.edu>
2025-01-14 16:39:34 -0600

    Deploy

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,17 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    distDir: 'dist',
-    rewrites: async () => {
-        return [
-          {
-            source: '/api/:path*',
-            destination:
-              process.env.NODE_ENV === 'development'
-                ? 'http://127.0.0.1:5328/api/:path*'
-                : '/api/',
-          },
-        ]
+  distDir: "dist",
+  output: "export",
+  rewrites: async () => {
+    return [
+      {
+        source: "/api/:path*",
+        destination:
+          process.env.NODE_ENV === "development"
+            ? "http://127.0.0.1:5328/api/:path*"
+            : "/api/",
       },
+    ];
+  },
 };
 
 export default nextConfig;
